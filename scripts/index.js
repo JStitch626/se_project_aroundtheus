d076324b1ac348ad70f0bb5157112602c51b64dd
MacPaca <macpaca@MacPacas-MacBook-Air.local>
2023-11-25 12:03:17 -0500

    Change objects in array back to 'name' change js line 67, 68 to cardData.name

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 const initialCards = [
   {
-    title: "Yosemite Valley",
+    name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
 
   {
-    title: "Lake Louise",
+    name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
 
   {
-    title: "Bald Mountains",
+    name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
 
   {
-    title: "Latemar",
+    name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
 
   {
-    title: "Vanoise National Park",
+    name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
 
   {
-    title: "Lago di Braies",
+    name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
@@ -64,8 +64,8 @@ function getCardElement(cardData) {
   const cardImageElement = cardElement.querySelector(".card__image");
   const cardTitleElement = cardElement.querySelector(".card__title");
   cardImageElement.setAttribute("src", cardData.link);
-  cardImageElement.setAttribute("alt", cardData.title);
-  cardTitleElement.textContent = cardData.title;
+  cardImageElement.setAttribute("alt", cardData.name);
+  cardTitleElement.textContent = cardData.name;
   return cardElement;
 }
 
