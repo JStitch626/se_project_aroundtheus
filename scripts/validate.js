a4106cfa9a4c1c47704d1feeed5a81665b91ca59
JStitch626 <j.o.sanchez591@gmail.com>
2024-07-02 21:02:33 -0400

    card modal change type to url

---
+++ scripts/validate.js
@@ -0,0 +1,11 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+});
