df52f22de558551aaa31ed04730aaf3b597d7cde
JStitch626 <j.o.sanchez591@gmail.com>
2024-07-10 22:14:51 -0400

    Add novalidate to HTML form element

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,15 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-enableValidation({
+const enableValidation = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
-});
+};
+
+// const checkInputValidity = ();
+// const hasInvalidInput = ();
+// const toggleButtonState = ();
