f421fa03dbe5e197c6554beaad16b40f7837bc2a
JStitch626 <j.o.sanchez591@gmail.com>
2024-07-10 23:33:24 -0400

    Add event listeners and handlers + CSS style

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,4 +1,35 @@
-// enabling validation by calling enableValidation()
+/* -------------------------------------------------------------------------- */
+/*                                  Elements                                  */
+/* -------------------------------------------------------------------------- */
+const formElement = document.querySelector(".form");
+const formInput = document.querySelector(".form__input");
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+/* -------------------------------------------------------------------------- */
+/*                                  Event Handlers                                 */
+/* -------------------------------------------------------------------------- */
+
+const showInputError = (element) => {
+  element.classList.add("form__input_type_error");
+  formError.classList.add("form__input-error_active");
+};
+
+const hideInputError = (element) => {
+  element.classList.remove("form__input_type_error");
+  formError.classList.remove("form__input-error_active");
+};
+/* -------------------------------------------------------------------------- */
+/*                               Event Listeners                              */
+/* -------------------------------------------------------------------------- */
+formElement.addEventListener("submit", () => {
+  evt.preventDefault();
+});
+
+formInput.addEventListener("input", () => {
+  evt.target.validity.valid;
+});
+
+/* -------------------------------------------------------------------------- */
 // pass all the settings on call
 
 const enableValidation = {
